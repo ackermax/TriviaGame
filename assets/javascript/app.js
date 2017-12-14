02afe88d28ce8c07bf9d67ba0e86fc22dc551eb6
Max Ackerman <max@ackermax.com>
2017-12-13 19:10:44 -0500

    started work on building the questions object

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// we're gonna try to build our game inside of this object and see what happens
+var quiz = {
+    questions: [
+        { question: "Who was the first HHN Icon?", a1: "Jack the Clown", a2: "The Crypt-Keeper", a3: "The Caretaker", a4: "Bloody Mary", correct: "a2", funFact: "The Crypt-Keeper originally appeared on HBO's Tales From the Crypt, which aired from 1989 to 1996.", imgLoc: "assets/images/cryptkeeper.gif" },
+        { question: "Who was the first HHN Icon?", a1: "Jack the Clown", a2: "The Crypt-Keeper", a3: "The Caretaker", a4: "Bloody Mary", correct: "a2", funFact: "The Crypt-Keeper originally appeared on HBO's Tales From the Crypt, which aired from 1989 to 1996.", imgLoc: "assets/images/cryptkeeper.gif" },
+        { question: "Who was the first HHN Icon?", a1: "Jack the Clown", a2: "The Crypt-Keeper", a3: "The Caretaker", a4: "Bloody Mary", correct: "a2", funFact: "The Crypt-Keeper originally appeared on HBO's Tales From the Crypt, which aired from 1989 to 1996.", imgLoc: "assets/images/cryptkeeper.gif" },
+        { question: "Who was the first HHN Icon?", a1: "Jack the Clown", a2: "The Crypt-Keeper", a3: "The Caretaker", a4: "Bloody Mary", correct: "a2", funFact: "The Crypt-Keeper originally appeared on HBO's Tales From the Crypt, which aired from 1989 to 1996.", imgLoc: "assets/images/cryptkeeper.gif" },
+        { question: "Who was the first HHN Icon?", a1: "Jack the Clown", a2: "The Crypt-Keeper", a3: "The Caretaker", a4: "Bloody Mary", correct: "a2", funFact: "The Crypt-Keeper originally appeared on HBO's Tales From the Crypt, which aired from 1989 to 1996.", imgLoc: "assets/images/cryptkeeper.gif" }
+    ]
+}
